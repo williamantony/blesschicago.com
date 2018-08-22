9ccb22adafd38559bb28aa5592c244041aadfc47
William Antony <william@willsquad.com>
2018-08-21 23:39:50 -0500

    Implemented Redux & modified styles in ContactFormInput

--- frontend/src/components/Contact/components/ContactForm/components/ContactFormInput/ContactFormInput.js
+++ frontend/src/components/Contact/components/ContactForm/components/ContactFormInput/ContactFormInput.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setContactInput } from '../../../../../../redux/actions';
 import './ContactFormInput.css';
 
 class ContactFormInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: `ContactForm_${ props.type }_${ props.name }_${ new Date().getTime() }`,
       type: props.type,
       name: props.name,
       label: props.label,
-      value: props.value,
+      value: props.value || '',
       status: {
         isFocused: false,
         isFilled: false,
-      }
+      },
     };
   }
 
-  handleInput = (event) => {
-    event.preventDefault();
-
-    const { value } = event.target;
+  componentWillReceiveProps(props) {
+    const value = props.contactForm[this.state.name] || '';
 
     this.setState({
       value,
@@ -29,6 +29,11 @@ class ContactFormInput extends Component {
         isFilled: (value === ''),
       },
     });
+  }
+
+  handleInput = (event) => {
+    event.preventDefault();
+    this.props.setContactInput(this.state.name, event.target.value);
   }
 
   handleFocus = (event) => {
@@ -53,6 +58,13 @@ class ContactFormInput extends Component {
     });
   }
 
+  shouldComponentUpdate(nextProp, nextState) {
+    const hasValueChanged = nextState.value !== this.state.value;
+    const hasFocusChanged = nextState.status.isFocused !== this.state.status.isFocused;
+
+    return (hasValueChanged || hasFocusChanged);
+  }
+
   render() {
     return (
       <div
@@ -60,13 +72,13 @@ class ContactFormInput extends Component {
         data-filled={this.state.status.isFilled}
         data-focused={this.state.status.isFocused}
       >
-        <label className="ContactFormInput__label" htmlFor="ContactFormName">{ this.state.label }</label>
+        <label className="ContactFormInput__label" htmlFor={this.state.id}>{ this.state.label }</label>
         {
           (this.state.type === 'textarea')
             ? (
               <textarea
                 type={this.state.type}
-                id="ContactFormName"
+                id={this.state.id}
                 className="ContactFormInput__input"
                 name={this.state.name}
                 value={this.state.value}
@@ -77,7 +89,7 @@ class ContactFormInput extends Component {
             ) : (
               <input
                 type={this.state.type}
-                id="ContactFormName"
+                id={this.state.id}
                 className="ContactFormInput__input"
                 name={this.state.name}
                 value={this.state.value}
@@ -95,11 +107,13 @@ class ContactFormInput extends Component {
 }
 
 const mapStateToProps = state => {
-  return state;
+  return {
+    contactForm: state.contactForm,
+  };
 };
 
 const mapDispatchToProps = {
-
+  setContactInput,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactFormInput);
