7380462e1f220b79f5f50c86237cc8f2a91900b5
William Antony <william@willsquad.com>
2018-08-24 00:35:35 -0500

    Refactored EventSchedule Component

--- frontend/src/components/EventSchedule/EventSchedule.js
+++ frontend/src/components/EventSchedule/EventSchedule.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './EventSchedule.css';
+import Heading from '../Heading/Heading';
 
 class EventSchedule extends Component {
   constructor(props) {
@@ -16,22 +17,22 @@ class EventSchedule extends Component {
         {
           dateTime: '1537572600000',
           name: 'Friday Evening',
-          place: '120 E Oak St,\nAddison, IL 60101',
+          place: '120 East Oak St,\nAddison, IL 60101',
         },
         {
           dateTime: '1537628400000',
           name: 'Special Prayer Seminar',
-          place: '120 E Oak St,\nAddison, IL 60101',
+          place: '120 East Oak St,\nAddison, IL 60101',
         },
         {
           dateTime: '1537659000000',
           name: 'Saturday Evening',
-          place: '120 E Oak St,\nAddison, IL 60101',
+          place: '120 East Oak St,\nAddison, IL 60101',
         },
         {
           dateTime: '1537745400000',
           name: 'Sunday Evening',
-          place: '120 E Oak St,\nAddison, IL 60101',
+          place: '120 East Oak St,\nAddison, IL 60101',
         }
       ],
       currentSlide: 0,
@@ -59,77 +60,79 @@ class EventSchedule extends Component {
       <div className="EventSchedule">
         <div className="EventSchedule__gradient" />
         <div className="EventSchedule__bg" />
-        <div className="EventSchedule__slider">
-          <div className="EventSchedule__slider-controls">
-            <div className="arrows">
-              <div className="arrow arrow-left" onClick={this.slideLeft} />
-              <div className="arrow arrow-right" onClick={this.slideRight} />
-            </div>
-          </div>
-          <div className="EventSchedule__overflow">
-            <div 
-              className="EventSchedule__content"
-              style={{
-                width: `${(100 / this.state.slidesPerView) * this.state.events.length}%`,
-                marginLeft: `-${ this.state.currentSlide * (100 / this.state.slidesPerView) }%`,
-              }}
-            >
-              {
-                this.state.events.map((event, index) => {
+        <div className="EventSchedule__content">
+          <div className="wrapper">
+            <Heading type="h2" text="Event Schedule" />
+            <div className="EventSlider">
+              <div className="EventSlider__controls">
+                <div className="arrows">
+                  <div className="arrow arrow-left" onClick={this.slideLeft} />
+                  <div className="arrow arrow-right" onClick={this.slideRight} />
+                </div>
+              </div>
+              <div className="EventSlider__overflow">
+                <div 
+                  className="EventSlider__content"
+                  style={{
+                    width: `${(100 / this.state.slidesPerView) * this.state.events.length}%`,
+                    marginLeft: `-${ this.state.currentSlide * (100 / this.state.slidesPerView) }%`,
+                  }}
+                >
+                  {
+                    this.state.events.map((event, index) => {
 
-                  const dateTime = new Date(parseInt(event.dateTime));
+                      const dateTime = new Date(parseInt(event.dateTime));
 
-                  const date = dateTime.getDate();
-                  const day = this.state.days[dateTime.getDay()];
-                  const month = this.state.months[dateTime.getMonth()];
-                  const year = dateTime.getFullYear();
+                      const date = dateTime.getDate();
+                      const day = this.state.days[dateTime.getDay()];
+                      const month = this.state.months[dateTime.getMonth()];
+                      const year = dateTime.getFullYear();
 
-                  const hours = dateTime.getHours();
-                  const hours12 = (hours > 12) ? hours - 12 : hours;
-                  const minutes = dateTime.getMinutes();
-                  const ampm = (hours >= 12) ? 'PM' : 'AM';
+                      const hours = dateTime.getHours();
+                      const hours12 = (hours > 12) ? hours - 12 : hours;
+                      const minutes = dateTime.getMinutes() < 10 ? `0${dateTime.getMinutes()}` : dateTime.getMinutes();
+                      const ampm = (hours >= 12) ? 'PM' : 'AM';
 
-                  const itemViewClass = ((index >= this.state.currentSlide) && (index < (this.state.currentSlide + this.state.slidesPerView))) ? 'EventSchedule__item--in-view' : '0.5';
+                      const itemClass = ((index >= this.state.currentSlide) && (index < (this.state.currentSlide + this.state.slidesPerView))) ? 'EventCard EventCard--in-view' : 'EventCard';
 
-                  return (
-                    <div
-                      key={index}
-                      className={`EventSchedule__item ${itemViewClass}`}
-                    >
-                      <div className="EventCard">
-                      
-                        <div className="EventCard__name">{ event.name }</div>
+                      return (
+                        <div key={index} className={itemClass}>
+                          <div className="EventCard__name">{ event.name }</div>
 
-                        <div className="EventCard__row">
-                          <div className="EventCard__label">Date</div>
-                          <div className="EventCard__value">
-                            { `${month} ${date}, ${year}` }
-                            <br />
-                            { `(${day})` }
+                          <div className="EventCard__row">
+                            <div className="EventCard__column">
+                              <div className="EventCard__label">Date</div>
+                              <div className="EventCard__value">
+                                { `${month} ${date}, ${year}` }
+                              </div>
+                            </div>
+
+                            <div className="EventCard__column">
+                              <div className="EventCard__label">Time</div>
+                              <div className="EventCard__value">{ `${hours12}:${minutes} ${ampm}` }
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                        <div className="EventCard__row">
-                          <div className="EventCard__label">Time</div>
-                          <div className="EventCard__value">{ `${hours12}:${minutes} ${ampm}` }
-                          </div>
-                        </div>
 
-                        <div className="EventCard__row">
-                          <div className="EventCard__label">Place</div>
-                          <div className="EventCard__value">
+                          <div className="EventCard__row">
+                            <div className="EventCard__column">
+                              <div className="EventCard__label">Place</div>
+                              <div className="EventCard__value">
                                 {
                                   event.place.split('\n').map((part, i) => {
                                     return <div key={i}>{ part }</div>;
                                   })
                                 }
+                              </div>
+                            </div>
                           </div>
-                        </div>
 
-                      </div>
-                    </div>
-                  );
-                })
-              }
+                        </div>
+                      );
+                    })
+                  }
+                </div>
+              </div>
             </div>
           </div>
         </div>
