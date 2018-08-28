125b01a6271279d4f85c63c4e0f0a25149af4bdf
William Antony <william@willsquad.com>
2018-08-28 13:21:56 -0500

    Refactored Cover Component

--- frontend/src/components/Cover/Cover.js
+++ frontend/src/components/Cover/Cover.js
@@ -2,34 +2,35 @@ import React, { Component } from 'react';
 import './Cover.css';
 import CoverInfo from './components/CoverInfo/CoverInfo';
 import CoverLogo from './components/CoverLogo/CoverLogo';
+import CoverBackgroundImage from './components/CoverBackgroundImage/CoverBackgroundImage';
+import CoverForegroundImage from './components/CoverForegroundImage/CoverForegroundImage';
 
 class Cover extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isBgVisible: false,
+      loaded: false,
     };
   }
   
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        isBgVisible: true,
+        loaded: true,
       });
     }, 0);
   }
 
   render() {
     return (
-      <div className="Cover" data-isbgvisible={this.state.isBgVisible}>
-        <div className="Cover__image" />
+      <div className="Cover" data-loaded={this.state.loaded}>
         <div className="Cover__dark-bg" />
         <div className="Cover__bg" />
-        <div className="Cover__image-pastor" />
         <div className="Cover__gradient" />
+        <CoverForegroundImage />
         <div className="Cover__area">
           <div className="wrapper">
-            <CoverLogo />
+            {/* <CoverLogo /> */}
             <CoverInfo />
           </div>
         </div>
