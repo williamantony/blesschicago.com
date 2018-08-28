8576b8ca33828a0828c1ce81cdcb91b518d8e254
William Antony <william@willsquad.com>
2018-08-28 13:17:01 -0500

    Added Header to LandingPage

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
+import Header from '../../components/Header/Header';
 import Cover from '../../components/Cover/Cover';
 import BibleVerse from '../../components/BibleVerse/BibleVerse';
 import EventSchedule from '../../components/EventSchedule/EventSchedule';
 import Location from '../../components/Location/Location';
 import Contact from '../../components/Contact/Contact';
-import Header from '../../components/Header/Header';
 
 class LandingPage extends Component {
 
   render() {
     return (
       <div className="Page LandingPage">
-        {/* <Header /> */}
+        <Header />
         <Cover />
         <BibleVerse />
         <EventSchedule />
